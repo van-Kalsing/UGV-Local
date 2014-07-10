22b17e9193b15a819254bef860390762c4ca8d09
van_Kalsing <kalsin@inbox.ru>
2014-07-10 12:11:31 +0600

    Планирование траектории: реализация механизма поиска в ширину

---
+++ planning/searching/searchers/tree_searchers/BF_searcher.py
@@ -0,0 +1,42 @@
+from planning.searching.searchers.tree_searchers.tree_searcher \
+	import TreeSearcher
+	
+	
+	
+	
+	
+	
+	
+class BFSearcher(TreeSearcher):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		
+		self.__perspective_states_records = []
+		
+		
+		
+		
+		
+	@property
+	def _contains_perspective_states_records(self):
+		return bool(self.__perspective_states_records)
+		
+		
+		
+	def _insert_perspective_state_record(self, perspective_state_record):
+		self.__perspective_states_records.append(
+			perspective_state_record
+		)
+		
+		
+		
+	def _retrieve_perspective_state_record(self):
+		if self.__perspective_states_records:
+			perspective_state_record = self.__perspective_states_records.pop(0)
+		else:
+			raise Exception() #!!!!! Генерировать хорошие исключения
+			
+			
+		return perspective_state_record
+		
